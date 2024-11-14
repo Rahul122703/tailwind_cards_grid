99f9b2e743a73a338c31d0bbe1e6d11772e3a1d0
rahul271203 <killkamam3@gmail.com>
2024-11-14 22:08:14 +0530

    adding project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"], // Path to your HTML files
+  darkMode: "class", // Enable dark mode with 'class' strategy
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
