5f99cd72ab8954f47dcf0c31127d7fbd3cba194a
rahul271203 <killkamam3@gmail.com>
2024-11-14 23:22:43 +0530

    copmlete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.html"], // Path to your HTML files
+  content: ["./*.html"], // Path to your HTML files
   darkMode: "class", // Enable dark mode with 'class' strategy
   theme: {
     extend: {},
